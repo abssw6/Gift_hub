640a9be65967edeb8dbff246bbc6ed9005f41fd4
Koji Samuels <kojisamuels@gmail.com>
2022-11-19 12:45:12 +0000

    have created a model and controller for EVENT

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,34 @@
+class EventsController < ApplicationController
+  def index
+    if params[:query].present?
+      @events = Event.where(name: params[:query])
+    else
+      @events = Events.all
+    end
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save!
+      redirect_to events_path(@event)
+    else
+      @event = Event.new
+      render 'event/show', status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :category, :title)
+  end
+end
---
+++ db/migrate/20221119123118_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.datetime :eventdate
+      t.string :category
+      t.string :title
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
