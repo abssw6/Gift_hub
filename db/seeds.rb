c32b2ebfc6d515432e11723c364322d8367b322a
Abdul Elmi <abs.sw6@gmail.com>
2022-11-19 14:34:44 +0000

    Users got names + seeded 10 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+
+puts "Creating 10 users"
+10.times do
+  object = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8))
+end
+puts "Done"
+
+
+
+# puts "Creating 50 drinks"
+# 10.times do
+#   file = URI.open(Faker::LoremFlickr.image(search_terms: ['sodacan']))
+#   i = 1
+#   object = Drink.new(
+#     name: Faker::Kpop.iii_groups,
+#     description: Faker::Lorem.sentences,
+#     stock_level: rand(50..100),
+#     price: rand(2..10),
+#     user: User.first
+#   )
