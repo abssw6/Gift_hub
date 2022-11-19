f3c5b9aab2c18d1d816ef661df7b3d2680db24bb
Gianluca Galli <gianluca.galli9519@gmail.com>
2022-11-19 12:40:47 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :events do
+    resources :whislists, only: :show do
+      resources :gifts, only: :show
+    end
+  end
 end
