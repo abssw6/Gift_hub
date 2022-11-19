1e5b4425bb0607a0933e4c9f9592a7edd5728c26
Koji Samuels <kojisamuels@gmail.com>
2022-11-19 15:24:02 +0000

    fixes to index controller and also the light layout for index html

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_123118) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_125405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,8 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_123118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
-   end
-   
+  end
+
   create_table "gifts", force: :cascade do |t|
     t.string "name"
     t.string "type"
