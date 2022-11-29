29064361fe8e23c2ab3598d74ad0d41e59c9df67
Abdul Elmi <abs.sw6@gmail.com>
2022-11-29 19:58:40 +0000

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :events do
-    resources :wishlists, only: [:show, :edit] do
-      resources :gifts_wishlists, only: [:create]
-      resources :gifts, only: :show do
-        member do
-          get 'commit_gift'
-        end
+
+  resources :wishlists, only: [:show, :edit] do
+    resources :gifts_wishlists, only: [:create]
+  end
+
+  resources :gifts_wishlists, only: :update
+
+  resources :gifts, only: :show do
+      member do
+        get 'commit_gift'
       end
     end
-  end
+
     resources :events do
       resources :invitations, only: :create
     end
   end
+
+  # migration to add user_id to gifts_wishlist
+  # add route for gifts_wishlists [update maybe a show as well]
+  # tidy up the app for the new routes
+  # then we need to add a form with hidden fields to gifts_wishlist
+  # make sure the routes are working
